e09e4c20c8ddbac1211949e851d5d8c5914d296a
david99pereyra <david99pereyra@gmail.com>
2023-05-17 18:37:51 -0300

    clase Jugador y ranking

---
+++ app/src/main/java/jgame23/Jugador.java
@@ -0,0 +1,28 @@
+package jgame23;
+
+public class Jugador {
+
+    private String nombre;
+    private int puntuacion;
+
+    public Jugador(String nombre){
+        this.nombre=nombre;
+        this.puntuacion=0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+}
